6f015e60aab2c0ac8c64b1e5741a3d0287dc54c5
Sam Hall <samuel.hall@helmholtz-berlin.com>
2024-07-05 12:03:37 +0200

    update

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import torch
 import numpy as np
 from bokeh.plotting import figure
 from bokeh.palettes import HighContrast3
+from bokeh.models import SingleIntervalTicker, LinearAxis, Span
 
 def pred_spec(model, index, test_dataset, graphnet):
     # --- Set the model to evaluation mode
@@ -79,3 +80,56 @@ def bokeh_spectra(ml_spectra, true_spectra):
     p.legend.label_text_font_size = '20px'
 
     return p
+
+def bokeh_hist(hist, edges, average, spacing):
+    p = figure(
+        x_axis_label = 'RSE value', y_axis_label = 'Frequency',
+        x_range = (edges[0], edges[-1]), y_range = (0, max(hist)+spacing),
+        width = 500, height = 450,
+        outline_line_color = 'black', outline_line_width = 2
+    )
+
+    p.toolbar.logo = None
+    p.toolbar_location = None
+    p.min_border = 25
+
+    # --- x-axis settings
+    p.xaxis.ticker.desired_num_ticks = 3
+    p.xaxis.axis_label_text_font_size = '24px'
+    p.xaxis.major_label_text_font_size = '24px'
+    p.xaxis.major_tick_in = 0
+    p.xaxis.major_tick_out = 10
+    p.xaxis.minor_tick_out = 6
+    p.xaxis.major_tick_line_width = 2
+    p.xaxis.minor_tick_line_width = 2
+    p.xaxis.major_tick_line_color = 'black'
+    p.xaxis.minor_tick_line_color = 'black'
+    p.xaxis[0].ticker.desired_num_ticks = 4
+    # --- y-axis settings
+    p.yaxis.axis_label_text_font_size = '24px'
+    p.yaxis.major_label_text_font_size = '24px'
+    p.yaxis.major_tick_in = 0
+    p.yaxis.major_tick_out = 10
+    p.yaxis.major_tick_line_width = 2
+    p.yaxis.major_tick_line_color = 'black'
+    p.yaxis.minor_tick_line_color = None
+    p.yaxis.major_label_text_color = 'black'
+    # --- grid settings
+    p.grid.grid_line_color = 'grey'
+    p.grid.grid_line_alpha = 0.3
+    p.grid.grid_line_width = 1.5
+    p.grid.grid_line_dash = "dashed"
+
+    # --- Format x-axis
+    ticker = SingleIntervalTicker(interval=20)
+    xaxis = LinearAxis(ticker=ticker)
+    p.add_layout(xaxis, 'below')
+
+    # --- Plot data
+    # --- Add histogram
+    p.quad(top=hist, bottom=0, left=edges[:-1], right=edges[1:], fill_color='skyblue', line_color='black')
+    # --- Add average line
+    vline = Span(location=average, dimension='height', line_color='black', line_width=3, line_dash='dashed')
+    p.renderers.extend([vline])
+
+    return(p)
